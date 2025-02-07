84e1010ed541df5fb4c68ca3f6e49457852bf033
Adam Faulkner <adamfaulkner1@gmail.com>
2025-02-07 11:28:07 -0800

    wip

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,4 +1,8 @@
-use std::{env, fs::File, io::Write};
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+};
 
 use graphics::{convert_image, DISPLAY_BUFFER_SIZE, DISPLAY_HEIGHT, DISPLAY_WIDTH};
 use image::{ImageReader, RgbImage};
@@ -7,18 +11,29 @@ mod graphics;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let input_filename = &args[1];
-    let output_filename = &args[2];
-    let img: RgbImage = ImageReader::open(input_filename)
-        .unwrap()
-        .decode()
-        .unwrap()
-        .thumbnail(DISPLAY_WIDTH as u32, DISPLAY_HEIGHT as u32)
-        .into();
-    let mut output_buffer = [0u8; DISPLAY_BUFFER_SIZE];
-    convert_image(&img, &mut output_buffer);
+    let input_file_base_path = &args[1];
+    let output_file_base_path = &args[2];
 
-    let mut file = File::create(output_filename).unwrap();
-    file.write_all(&output_buffer).unwrap();
+    for (idx, entry_result) in fs::read_dir(input_file_base_path).unwrap().enumerate() {
+        let entry = entry_result.unwrap();
+        let path = entry.path();
+        if path.is_dir() {
+            continue;
+        }
+
+        let img: RgbImage = ImageReader::open(path)
+            .unwrap()
+            .decode()
+            .unwrap()
+            .thumbnail(DISPLAY_WIDTH as u32, DISPLAY_HEIGHT as u32)
+            .into();
+        let mut output_buffer = [0u8; DISPLAY_BUFFER_SIZE];
+        convert_image(&img, &mut output_buffer);
+
+        let output_path = format!("{}/{}.inkyframe", output_file_base_path, idx);
+
+        let mut file = File::create(output_path).unwrap();
+        file.write_all(&output_buffer).unwrap();
+    }
     println!("done");
 }
